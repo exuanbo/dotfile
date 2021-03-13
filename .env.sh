b647553f44c1c0cffffa6fef1e5bfc25ed594825
exuanbo <exuanbo@protonmail.com>
2021-03-13 12:39:03 +0000

    update

--- .env.sh
+++ .env.sh
@@ -1,24 +1,7 @@
-# n
-export N_PREFIX="$HOME/n"
-export N_PRESERVE_NPM=1
-export PATH="$N_PREFIX/bin:$PATH"
-
-# rust
+# rust cargo
 export PATH="$HOME/.cargo/bin:$PATH"
 
-# mono
-export MONO_GAC_PREFIX="/usr/local"
-
-# haskell ghcup
-export PATH="$HOME/.cabal/bin:$HOME/.ghcup/bin:$PATH"
-
-# ruby-build
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
-
-# rbenv
-eval "$(rbenv init -)"
-
-# conda
+# python conda
 __conda_setup="$('$HOME/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
@@ -31,3 +14,22 @@ else
 fi
 unset __conda_setup
 
+# haskell ghcup
+export PATH="$HOME/.cabal/bin:$HOME/.ghcup/bin:$PATH"
+
+# csharp mono
+export MONO_GAC_PREFIX="/usr/local"
+
+# node n
+export N_PREFIX="$HOME/n"
+export N_PRESERVE_NPM=1
+export PATH="$N_PREFIX/bin:$PATH"
+
+# ocaml opam
+test -r $HOME/.opam/opam-init/init.zsh && . $HOME/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
+
+# ruby-build
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+
+# ruby rbenv
+eval "$(rbenv init -)"
